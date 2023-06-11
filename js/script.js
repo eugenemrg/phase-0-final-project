6ccec0e39b4da7b565d188e76ecb3d543af6652f
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-06-11 03:14:14 +0300

    Removed first JS carousel implementation for testimonial section in favour of implementation two

--- js/script.js
+++ js/script.js
@@ -4,29 +4,6 @@ let activeCardsDisplayed = document.querySelectorAll('.active-testimonial');
 let indicators = document.querySelectorAll('.indicator');
 let activeIndicator = document.querySelectorAll('.active-indicator');
 
-//implementation-1 
-
-setInterval(() => {
-    if (activeCardsDisplayed[0].nextElementSibling !== null) {
-        let nextCard = activeCardsDisplayed[0].nextElementSibling;
-        let nextIndicator = activeIndicator[0].nextElementSibling;
-
-        activeCardsDisplayed[0].classList.toggle('active-testimonial');
-        nextCard.classList.toggle('active-testimonial');
-
-        activeIndicator[0].classList.toggle('active-testimonial');
-        nextIndicator.classList.toggle('active-indicator');
-    } else {
-        activeCardsDisplayed[0].classList.toggle('active-testimonial');
-        activeIndicator[0].classList.toggle('active-indicator');
-
-        cards[0].classList.toggle('active-testimonial');
-        indicators[0].classList.toggle('active-indicator');
-    }
-}, 200000);
-
-// implementation-2
-
 setInterval(() => {
     let activeCardIndex = [...cards].indexOf(activeCardsDisplayed[0]);
     let currentCard = activeCardIndex + 1;
